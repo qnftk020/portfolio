630007c208a104ab8a089a4a475643aa1100fd71
YeonghwanShin <102016663+YeongHwanShin@users.noreply.github.com>
2026-02-22 01:39:23 +0900

    Update next.config.js

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // GitHub Pages에서 커스텀 도메인 없이 배포할 경우:
-  // basePath: '/repository-name',
-  // assetPrefix: '/repository-name/',
+  // basePath: '/portfolio',
+  // assetPrefix: '/portfolio/',
   trailingSlash: true,
   images: {
     unoptimized: true,
